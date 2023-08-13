db87aa7749368b8c2cac0050174ce62819337c5d
Leopard <137736066+leopard1337@users.noreply.github.com>
2023-08-13 03:52:51 +0200

    Update ADV_Web_Scraper.py

--- ADV_Web_Scraper.py
+++ ADV_Web_Scraper.py
@@ -1,61 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+import scrapy
 import json
 
-# Make a request to the website with a User-Agent header
-url = 'INPUT_URL'
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+    start_urls = ['INPUT_URL']
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
-}
-response = requests.get(url, headers=headers)
+    def parse(self, response):
+        page_title = response.xpath('//title/text()').get().strip()
+        validated_links = response.css('a[href^="http"]::attr(href)').getall()
 
-# Parse the HTML content
-soup = BeautifulSoup(response.content, 'html.parser')
+        linked_page_titles = []
+        for link in validated_links:
+            yield scrapy.Request(link, callback=self.parse_linked_page, cb_kwargs={'linked_page_titles': linked_page_titles})
 
-# Extract the page title
-page_title = soup.title.string.strip() if soup.title else 'Title not found'
+        header_texts = response.css('h1::text').getall()
 
-# Extract all valid links
-validated_links = []
-for link in soup.find_all('a'):
-    href = link.get('href')
-    if href and href.startswith('http'):
-        validated_links.append(href)
+        data = {
+            'page_title': page_title,
+            'urls': [{'url': url, 'title': title} for url, title in zip(validated_links, linked_page_titles)],
+            'headers': header_texts
+        }
 
-# Extract the titles of linked pages
-linked_page_titles = []
-for link in validated_links:
-    linked_response = requests.get(link)
-    linked_soup = BeautifulSoup(linked_response.content, 'html.parser')
-    linked_title = linked_soup.title.string.strip() if linked_soup.title else 'Title not found'
-    linked_page_titles.append(linked_title)
+        with open('scraped_data.json', 'w') as json_file:
+            json.dump(data, json_file, indent=2)
 
-# Extract header texts
-header_texts = [header.text.strip() for header in soup.find_all('h1')]
+        self.log("Web scraping completed. Data captured and saved to 'scraped_data.json'.")
 
-# Create a dictionary to store the captured data
-data = {
-    'page_title': page_title,
-    'urls': [{'url': normalized_url, 'title': linked_page_title} for normalized_url, linked_page_title in zip(validated_links, linked_page_titles)],
-    'headers': header_texts
-}
-
-# Save the data to a text file
-with open('scraped_data.txt', 'w') as file:
-    file.write(str(data))
-
-# Save the data to a CSV file
-df = pd.DataFrame(data['urls'])
-df.to_csv('scraped_data.csv', index=False)
-
-# Save the data to a JSON file
-with open('scraped_data.json', 'w') as json_file:
-    json.dump(data, json_file)
-
-# Perform data analysis on captured data
-df = pd.DataFrame(data['urls'])
-print(df.head())  # Example analysis using Pandas
-
-print("Web scraping completed. Data captured and saved to 'scraped_data.txt', 'scraped_data.csv', and 'scraped_data.json'.")
+    def parse_linked_page(self, response, linked_page_titles):
+        linked_title = response.xpath('//title/text()').get().strip()
+        linked_page_titles.append(linked_title)
